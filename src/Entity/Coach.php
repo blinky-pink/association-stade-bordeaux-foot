838598d8fb6c2d7a2a392c905ad23091e0f7db4b
blinky.pink <ed_coste@hotmail.com>
2026-04-28 11:39:02 +0200

    Initialisation projet Symfony + MySQL + entity Coach

---
+++ src/Entity/Coach.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CoachRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CoachRepository::class)]
+class Coach
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Licence = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): static
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getLicence(): ?string
+    {
+        return $this->Licence;
+    }
+
+    public function setLicence(?string $Licence): static
+    {
+        $this->Licence = $Licence;
+
+        return $this;
+    }
+}
