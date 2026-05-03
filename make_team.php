0d257307c4d46b546dea9023c40d77018a9fd755
blinky.pink <ed_coste@hotmail.com>
2026-05-03 10:44:18 +0200

    Ajout TeamType, create/edit/delete team

---
+++ make_team.php
@@ -0,0 +1,40 @@
+<?php
+$content = <<<'PHP'
+<?php
+
+namespace App\Form;
+
+use App\Entity\Team;
+use App\Entity\Coach;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TeamType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Name', TextType::class, ['label' => 'Nom'])
+            ->add('Coach', EntityType::class, [
+                'class' => Coach::class,
+                'choice_label' => 'name',
+                'label' => 'Coach',
+                'required' => false,
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Enregistrer'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(['data_class' => Team::class]);
+    }
+}
+PHP;
+
+file_put_contents('src/Form/TeamType.php', $content);
+echo "OK !";
--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Team;
+use App\Form\TeamType;
 use App\Repository\TeamRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -20,7 +24,7 @@ class TeamController extends AbstractController
         ]);
     }
 
-    // Affiche le détail d'une équipe avec ses joueurs
+    // Affiche le détail d'une équipe
     #[Route('/team/{id}', name: 'team_show')]
     public function show(int $id, TeamRepository $teamRepository): Response
     {
@@ -33,5 +37,66 @@ class TeamController extends AbstractController
         return $this->render('team/show.html.twig', [
             'team' => $team,
         ]);
+    }
+
+    // Créer une nouvelle équipe
+    #[Route('/teams/create', name: 'team_create')]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $team = new Team();
+        $form = $this->createForm(TeamType::class, $team);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($team);
+            $em->flush();
+
+            return $this->redirectToRoute('team_list');
+        }
+
+        return $this->render('team/create.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    // Modifier une équipe existante
+    #[Route('/teams/edit/{id}', name: 'team_edit')]
+    public function edit(int $id, Request $request, TeamRepository $teamRepository, EntityManagerInterface $em): Response
+    {
+        $team = $teamRepository->find($id);
+
+        if (!$team) {
+            throw $this->createNotFoundException('Équipe introuvable');
+        }
+
+        $form = $this->createForm(TeamType::class, $team);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('team_list');
+        }
+
+        return $this->render('team/edit.html.twig', [
+            'form' => $form,
+            'team' => $team,
+        ]);
+    }
+
+    // Supprimer une équipe
+    #[Route('/teams/delete/{id}', name: 'team_delete')]
+    public function delete(int $id, TeamRepository $teamRepository, EntityManagerInterface $em): Response
+    {
+        $team = $teamRepository->find($id);
+
+        if (!$team) {
+            throw $this->createNotFoundException('Équipe introuvable');
+        }
+
+        $em->remove($team);
+        $em->flush();
+
+        return $this->redirectToRoute('team_list');
     }
 }
